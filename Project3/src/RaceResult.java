ba774322e322feb412815a92d1f5c874a721aa85
jpjcode <jpjoseph8145@gmail.com>
2026-03-28 13:46:32 -0400

    Modified RaceResult.java

--- Project3/src/RaceResult.java
+++ Project3/src/RaceResult.java
@@ -1,2 +1,19 @@
+//at the add more to this later
 public class RaceResult {
+    private Car winner;
+    private double time;
+    
+    public RaceResult() {
+
+    }
+
+    public void setTime(double time) {
+        this.time = time;
+    }
+
+    public void setWinner(Car winner) {
+        this.winner = winner;
+    }
+
+    
 }
